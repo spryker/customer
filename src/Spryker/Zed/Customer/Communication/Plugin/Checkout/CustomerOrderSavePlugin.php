a9ded6619defda860f7309a6bd05b37d17242f7e
Denis Turkov <denis.turkov@spryker.com>
2017-11-02 16:37:21 +0000

    CORE-1296: Refactor doSave plugins to use a SaveOrderTransfer directly and do not change the checkout response

--- src/Spryker/Zed/Customer/Communication/Plugin/Checkout/CustomerOrderSavePlugin.php
+++ src/Spryker/Zed/Customer/Communication/Plugin/Checkout/CustomerOrderSavePlugin.php
@@ -5,30 +5,28 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\Customer\Communication\Plugin;
+namespace Spryker\Zed\Customer\Communication\Plugin\Checkout;
 
 use Generated\Shared\Transfer\CheckoutResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
-use Spryker\Zed\Checkout\Dependency\Plugin\CheckoutSaveOrderInterface;
+use Generated\Shared\Transfer\SaveOrderTransfer;
+use Spryker\Zed\Checkout\Dependency\Plugin\PlaceOrder\CheckoutSaveOrderInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
- * @deprecated Use \Spryker\Zed\Customer\Communication\Plugin\Checkout\CustomerOrderSavePlugin instead
- * Will be removed with the next major.
- *
  * @method \Spryker\Zed\Customer\Business\CustomerFacade getFacade()
  * @method \Spryker\Zed\Customer\Communication\CustomerCommunicationFactory getFactory()
  */
-class OrderCustomerSavePlugin extends AbstractPlugin implements CheckoutSaveOrderInterface
+class CustomerOrderSavePlugin extends AbstractPlugin implements CheckoutSaveOrderInterface
 {
     /**
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
-     * @param \Generated\Shared\Transfer\CheckoutResponseTransfer $checkoutResponse
+     * @param SaveOrderTransfer $saveOrderTransfer
      *
      * @return void
      */
-    public function saveOrder(QuoteTransfer $quoteTransfer, CheckoutResponseTransfer $checkoutResponse)
+    public function saveOrder(QuoteTransfer $quoteTransfer, SaveOrderTransfer $saveOrderTransfer)
     {
-        $this->getFacade()->saveCustomerForOrder($quoteTransfer, $checkoutResponse);
+        $this->getFacade()->saveOrderCustomer($quoteTransfer, $saveOrderTransfer);
     }
 }
