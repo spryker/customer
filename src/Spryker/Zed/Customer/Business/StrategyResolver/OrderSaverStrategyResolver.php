890eaef266f9e5d1db5f273b4258538b9eb98371
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-01-11 21:04:58 +0200

    CC-2253: Added new "CustomerService". Updated "CustomerFacade".
    * Added new service "CustomerService" to check new multi shipment functionality is turned on.
    * Updated "CustomerFacade" with new multi shipment functionality resolver.
    * Added new "CustomerOrderSaverWithMultiShippingAddress" with new multi shipment functionality.
    * Created new "OrderSaverStrategyResolver" and "OrderSaverStrategyResolverInterface" to resolve BC changes for multiply shipment.
    * Deprecated old "CustomerOrderSaver".
    * Updated "CustomerDependencyProvider".
    * Updated several "transfer.xml" files according to usage of properties of multiple shipment.

---
+++ src/Spryker/Zed/Customer/Business/StrategyResolver/OrderSaverStrategyResolver.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Customer\Business\StrategyResolver;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Zed\Kernel\Exception\Container\ContainerKeyNotFoundException;
+use Spryker\Zed\Customer\Business\Checkout\CustomerOrderSaverInterface;
+use Spryker\Zed\Customer\Dependency\Service\CustomerToCustomerServiceInterface;
+
+/**
+ * @deprecated Remove strategy resolver after multiple shipment will be released.
+ */
+class OrderSaverStrategyResolver implements OrderSaverStrategyResolverInterface
+{
+    /**
+     * @var \Spryker\Zed\Customer\Dependency\Service\CustomerToCustomerServiceInterface
+     */
+    protected $service;
+
+    /**
+     * @var \Spryker\Zed\Sales\Business\Order\SalesOrderSaverInterface[]
+     */
+    protected $strategyContainer;
+
+    /**
+     * @throws \Spryker\Zed\Kernel\Exception\Container\ContainerKeyNotFoundException
+     *
+     * @param \Spryker\Zed\Customer\Dependency\Service\CustomerToCustomerServiceInterface $service
+     * @param array|\Spryker\Zed\Customer\Business\Checkout\CustomerOrderSaverInterface[] $strategyContainer
+     */
+    public function __construct(CustomerToCustomerServiceInterface $service, array $strategyContainer)
+    {
+        $this->service = $service;
+        $this->strategyContainer = $strategyContainer;
+
+        $this->assertRequiredStrategyContainerItems();
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Spryker\Zed\Customer\Business\Checkout\CustomerOrderSaverInterface
+     */
+    public function resolveByQuote(QuoteTransfer $quoteTransfer): CustomerOrderSaverInterface
+    {
+        if ($this->service->checkQuoteItemHasOwnShipmentTransfer($quoteTransfer) === false) {
+            return $this->strategyContainer[static::STRATEGY_KEY_WITHOUT_MULTI_SHIPMENT];
+        }
+
+        return $this->strategyContainer[static::STRATEGY_KEY_WITH_MULTI_SHIPMENT];
+    }
+
+    /**
+     * @throws \Spryker\Zed\Kernel\Exception\Container\ContainerKeyNotFoundException
+     */
+    protected function assertRequiredStrategyContainerItems(): void
+    {
+        if (!isset($this->strategyContainer[static::STRATEGY_KEY_WITHOUT_MULTI_SHIPMENT])) {
+            throw new ContainerKeyNotFoundException($this, static::STRATEGY_KEY_WITHOUT_MULTI_SHIPMENT);
+        }
+
+        if (!isset($this->strategyContainer[static::STRATEGY_KEY_WITH_MULTI_SHIPMENT])) {
+            throw new ContainerKeyNotFoundException($this, static::STRATEGY_KEY_WITH_MULTI_SHIPMENT);
+        }
+    }
+}
